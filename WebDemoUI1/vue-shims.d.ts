081be37151b76ddbf05f27affecb279d84c22543
NTS-NguyenXuanThanh <thanhnx.hust@gmail.com>
2023-09-27 14:34:08 +0700

    remove demo1

--- WebDemoUI1/vue-shims.d.ts
+++
@@ -1,6 +0,0 @@
- //Tell the TypeScript compiler that importing .vue files is OK
- declare module "*.vue" {
-  import { defineComponent } from "vue";
-  const component: ReturnType<typeof defineComponent>;
-  export default component;
-}
